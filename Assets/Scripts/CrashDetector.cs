c496f2976e49179a4908b372352a085726ab9ba7
Jonathan Dray <jonathan.dray@inspiralytics.com>
2021-11-11 11:03:01 +0000

    Added Crash Event
    - Added Crash Script
    - Tagged terrain as "Ground"
    - Changed circle collider on player head to trigger
    - attached crash script to player

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class CrashDetector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag =="Ground")
+        {
+            Debug.Log("Ouch!");
+        }
     }
 }
