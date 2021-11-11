a9b97ac6268361f6de6af7df1debac51d218f30e
Jonathan Dray <jonathan.dray@inspiralytics.com>
2021-11-11 10:18:40 +0000

    Amended PlayerMovement Else If

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,8 +21,7 @@ public class PlayerController : MonoBehaviour
         {
             rb2d.AddTorque(torqueAmount);
         }
-
-        if(Input.GetKey(KeyCode.RightArrow))
+        else if(Input.GetKey(KeyCode.RightArrow))
         {
             rb2d.AddTorque(-torqueAmount);
         }
