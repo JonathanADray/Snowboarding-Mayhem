905e812f9a2f317b869952491a2110bf522d1def
Jonathan Dray <jonathan.dray@inspiralytics.com>
2021-11-11 15:06:54 +0000

    Added Crash and Finish Particle Effect to Trigger upon collisionr

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -7,10 +7,13 @@ public class CrashDetector : MonoBehaviour
 {
 
     [SerializeField] float delayTime = 0.5f;
+
+    [SerializeField] ParticleSystem crashEffect;
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag =="Ground")
         {
             Debug.Log("Ouch!");
+            crashEffect.Play();
             Invoke("ReloadScene",delayTime);
         }
     }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -7,10 +7,12 @@ public class FinishLine : MonoBehaviour
 {
 
     [SerializeField] float delayTime = 2f;
+    [SerializeField] ParticleSystem finishEffect;
     void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Player")
         {
             Debug.Log("You Finished!");
+            finishEffect.Play();
             Invoke("ReloadScene", delayTime); // Invoke has to use a method that you are delaying
         }
     }
