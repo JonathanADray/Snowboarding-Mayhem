87b56f6efb388c07a0fe388644ede7714cb372ad
Jonathan Dray <jonathan.dray@inspiralytics.com>
2021-11-11 15:50:26 +0000

    Added Ability to boost player
    Boost ability added by manipulating the speed on SurfaceEffector - using find in type in script to manipulate

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,27 +5,47 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount= 1f;
+    [SerializeField] float normalSpeed=20f;
+    [SerializeField] float boostSpeed=40f;
     Rigidbody2D rb2d;
-
+    SurfaceEffector2D surfaceEffector2D;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        RotatePlayer();
+        RespondToBoost();
+
+    }
+
+    void RespondToBoost()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else 
+        {
+            surfaceEffector2D.speed = normalSpeed;
+        }
+    }
+
+    void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb2d.AddTorque(torqueAmount);
         }
-        else if(Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             rb2d.AddTorque(-torqueAmount);
         }
-
-
     }
 }
